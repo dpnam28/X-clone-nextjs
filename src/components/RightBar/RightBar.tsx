79e940c0ea49370866743759fb86e0984f0764d0
Duong Nam <dnam281224@gmail.com>
2025-10-02 16:01:54 +0700

    made rightbar scrollable

--- src/components/RightBar/RightBar.tsx
+++ src/components/RightBar/RightBar.tsx
@@ -3,23 +3,28 @@ import Search from "./Search";
 import PopularTags from "./PopularTags";
 import Recommendations from "./Recommendations";
 import Link from "next/link";
+import { ScrollArea } from "../ui/scroll-area";
 
 const RightBar = () => {
   return (
     <div className="pt-4 flex flex-col gap-4 sticky top-0 h-max">
       <Search />
-      <PopularTags />
-      <Recommendations />
+      <ScrollArea className="h-[calc(100vh_-_5rem)] overflow-y-auto">
+        <div className="pt-4 flex flex-col gap-4 sticky top-0 h-max">
+          <PopularTags />
+          <Recommendations />
 
-      {/* Footer */}
-      <div className="text-gray text-sm flex gap-x-4 flex-wrap">
-        <Link href="/">Terms of Service</Link>
-        <Link href="/">Privacy Policy</Link>
-        <Link href="/">Cookie Policy</Link>
-        <Link href="/">Accessibility</Link>
-        <Link href="/">Ads Info</Link>
-        <span>&copy; 2025 copyright</span>
-      </div>
+          {/* Footer */}
+          <div className="text-gray text-sm flex gap-x-4 flex-wrap">
+            <Link href="/">Terms of Service</Link>
+            <Link href="/">Privacy Policy</Link>
+            <Link href="/">Cookie Policy</Link>
+            <Link href="/">Accessibility</Link>
+            <Link href="/">Ads Info</Link>
+            <span>&copy; 2025 copyright</span>
+          </div>
+        </div>
+      </ScrollArea>
     </div>
   );
 };
